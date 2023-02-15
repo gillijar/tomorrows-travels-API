cfb1010a67d71be729874f64fcdaab74a6980775
Jarrod Gilliland <gillijar@gmail.com>
2023-02-15 13:32:18 -0500

    fixed auth bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,35 +4,6 @@ const AppError = require("../utilities/appError");
 const catchAsync = require("../utilities/catchAsync");
 const sendEmail = require("../utilities/email");
 
-const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-};
-
-const createSendToken = (user, statusCode, res) => {
-  const token = signToken(user._id);
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
-  res.cookie("jwt", token, cookieOptions);
-
-  user.password = undefined;
-
-  res.status(statusCode).json({
-    status: "success",
-    token,
-    data: {
-      user,
-    },
-  });
-};
-
 exports.signup = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
 
@@ -59,16 +30,12 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  createSendToken(newUser, 201, res);
-  // const token = signToken(newUser._id);
-
-  // res.status(201).json({
-  //   status: "success",
-  //   token,
-  //   data: {
-  //     user: newUser,
-  //   },
-  // });
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -84,17 +51,15 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError("Incorrect email or password.", 401));
   }
 
-  createSendToken(user, 200, res);
-  // const token = signToken(user._id);
-  // res.status(200).json({
-  //   status: "success",
-  //   token,
-  //   user: {
-  //     name: user.name,
-  //     email: user.email,
-  //     id: user.id,
-  //   },
-  // });
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: {
+        id: user.id,
+        role: user.role,
+      },
+    },
+  });
 });
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
